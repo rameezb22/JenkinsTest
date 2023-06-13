3b5774aca68344194aca664332eb69871d8f0975
rameezb22 <rameez.b.bhat@gmail.com>
2023-06-13 15:58:27 +0530

    26

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
 from selenium import webdriver
 
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-# Set up the desired capabilities
-capabilities = DesiredCapabilities.CHROME
-
-# Set up the remote WebDriver
-driver = webdriver.Remote("https://jenkins.lockular.in:4444/wd/hub", capabilities)
+driver = webdriver.Chrome()
