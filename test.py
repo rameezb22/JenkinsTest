9144d0819f58d5f45ca64606b0124e171751b8b9
rameezb22 <rameez.b.bhat@gmail.com>
2023-06-13 17:02:34 +0530

    33

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
 
 driver.get("https://sabien-dasbhoard.lockular.ai/")
