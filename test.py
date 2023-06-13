310dcfeb8ca65bb082f5e74044be571b13642bcf
rameezb22 <rameez.b.bhat@gmail.com>
2023-06-13 17:35:56 +0530

    75

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 cchrome_options = webdriver.ChromeOptions()
 cchrome_options.add_argument('--headless')
