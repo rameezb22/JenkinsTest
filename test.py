9c2537f0dde859bc5bd0664939a8a38ec20cbb9a
rameezb22 <rameez.b.bhat@gmail.com>
2023-06-13 12:43:48 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,22 @@
+# Install Selenium package
+pip install selenium
+
+from selenium import webdriver
+
+# Set up the browser driver
+driver = webdriver.Chrome('/path/to/chromedriver')
+
+# Navigate to the webpage
+driver.get('http://example.com')
+
+# Wait for the page to fully load
+driver.implicitly_wait(10)  # Adjust the timeout as needed
+
+# Fetch the HTML data
+html_data = driver.page_source
+
+# Print or process the HTML data as desired
+print(html_data)
+
+# Close the browser
+driver.quit()
