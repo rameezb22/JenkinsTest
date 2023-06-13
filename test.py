195c2725076534b36190b29f5d6cda5c6971de07
rameezb22 <rameez.b.bhat@gmail.com>
2023-06-13 15:47:18 +0530

    20

--- test.py
+++ test.py
@@ -1,19 +1,1 @@
-
-
 from selenium import webdriver
-
-driver = webdriver.Firefox('/home/user/drivers/firefoxdriver')
-# Navigate to the webpage
-driver.get('http://m2g-cloud.lockular.ai')
-
-# Wait for the page to fully load
-driver.implicitly_wait(10)  # Adjust the timeout as needed
-
-# Fetch the HTML data
-html_data = driver.page_source
-
-# Print or process the HTML data as desired
-print(html_data)
-
-# Close the browser
-driver.quit()
