8278daf1cd2700c113e6d7cef9bcebba685629c2
rameezb22 <rameez.b.bhat@gmail.com>
2023-06-13 17:24:09 +0530

    65

--- test.py
+++ test.py
@@ -8,11 +8,18 @@ cchrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=cchrome_options)
 
 
-driver.get("https://m2gcloud.lockular.ai/")
+driver.get("https://m2gcloud-staging.lockular.ai")
 
 
-sleep(1000);  # Adjust the timeout as needed
+driver.implicitly_wait(200)  # Adjust the timeout as needed
 
+
+username_input = driver.find_element_by_id('username')
+password_input = driver.find_element_by_id('password')
+
+
+username_input.send_keys('rameezb')
+password_input.send_keys('lockular')
 # Fetch the HTML data
 html_data = driver.page_source
 
